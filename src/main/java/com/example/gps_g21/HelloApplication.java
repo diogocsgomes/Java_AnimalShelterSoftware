8623d64e4d98e495f19a2c852d56671f898bc309
diogocsgomes <diogocsgomes@gmail.com>
2023-09-22 14:30:27 +0100

    Filtarção do input feita

--- src/main/java/com/example/gps_g21/HelloApplication.java
+++ src/main/java/com/example/gps_g21/HelloApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,8 +19,102 @@ public class HelloApplication extends Application {
     }
 
 
+    public static boolean isOperator(char input){
+
+        return input == '+' || input == '-' || input == '*' || input == '/'
+                || input == '^';
+
+    }
+
+    public static boolean isDecimalPoint(char input){
+
+        return input == '.' || input == ',';
+
+    }
+
+    public static boolean isValidInput(String input){
+
+        int numOperators = 0;
+        int numDigits = 0;
+        int decimalPoints = 0;
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(isOperator(input.charAt(i)))
+                numOperators++;
+            if(Character.isDigit(input.charAt(i)))
+                numDigits++;
+            if(isDecimalPoint(input.charAt(i)))
+                decimalPoints++;
+        }
+
+        if(input.length() != numOperators + numDigits + decimalPoints) {
+            //(input.length() != numOperators + numDigits + decimalPoints) ->significa que existem caraccteres invalidos
+
+            return false;
+
+        }
+
+        if(isOperator(input.charAt(0)) || isOperator(input.charAt(input.length() - 1))
+                || isDecimalPoint(input.charAt(0)) || isDecimalPoint(input.charAt(input.length() - 1)))
+            return false; //verifica se o primeiro e ultimo operadores sao invalidos
+
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            //NÃO É PERCISO PRECUPAR COM OPERAÇOES DO TIPO INDEX = -1 OU INDEX > LENGTH PQ A OPERAÇÃO ANTERIOIR JA FILTROU ISSO
+
+            if(isDecimalPoint(input.charAt(i)))
+            {
+                if(isDecimalPoint(input.charAt(i -1)) || isDecimalPoint(input.charAt(i + 1)))
+                    return false; // verifica se existem 2 "." juntos
+
+                if(isOperator(input.charAt( i - 1)) || isOperator(input.charAt(i + 1)))
+                {
+                    return false; //verifica se existem operadores e pontos juntos "+."
+                }
+            }
+
+            if(isOperator(input.charAt(i)))
+            {
+                if(isOperator(input.charAt(i - 1)) || isOperator(input.charAt(i + 1)))
+                {
+                    return false; // verifica se existem operadores juntos "--"
+                }
+            }
+
+
+        }
+        return true;
+
+
+    }
+
 
     public static void main(String[] args) {
-        launch();
+        //launch();
+        Scanner in = new Scanner(System.in);
+        String input = new String();
+
+        while (true)
+        {
+            System.out.print("\n-> ");
+            input = in.nextLine();
+            if(input.equalsIgnoreCase("sair"))
+                break;
+            if(isValidInput(input))
+                System.out.println("Input valido :)) " + input);
+            else
+                System.out.println("Input invalido :((( " + input);
+
+
+        }
+
+
+        System.out.println("Sai");
+
+
+
+
     }
 }
