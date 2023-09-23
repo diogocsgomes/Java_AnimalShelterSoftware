9ddfefc101ef24bce6923189e01627c4e1bc04c3
Bruno Fonseca <a2021129658@isec.pt>
2023-09-23 15:15:08 +0100

    Simplificacao do codigo

--- src/main/java/com/example/gps_g21/HelloApplication.java
+++ src/main/java/com/example/gps_g21/HelloApplication.java
@@ -1,10 +1,8 @@
 package com.example.gps_g21;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -18,78 +16,24 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    public static float isValidInput(String input){
+        String[] parts = input.split("[+\\-*/^]");
+        if (parts.length != 2)//Verifica se tem dois números ou não
+            return 0;
 
-    public static boolean isOperator(char input){
+        try{
+            float num1 = Float.parseFloat(parts[0]);//Tranformar a string em float
+            float num2 = Float.parseFloat(parts[1]);
+            char operation = input.replaceAll("[0-9.]", "").charAt(0);//Limpa todos os numeros e '.' da string deixando apenas o operador
+            if(operation == '+')
+                    return num1 + num2;
+            else
+                return 0;
 
-        return input == '+' || input == '-' || input == '*' || input == '/'
-                || input == '^';
-
-    }
-
-    public static boolean isDecimalPoint(char input){
-
-        return input == '.' || input == ',';
-
-    }
-
-    public static boolean isValidInput(String input){
-
-        int numOperators = 0;
-        int numDigits = 0;
-        int decimalPoints = 0;
-
-        for(int i = 0; i < input.length(); i++)
-        {
-            if(isOperator(input.charAt(i)))
-                numOperators++;
-            if(Character.isDigit(input.charAt(i)))
-                numDigits++;
-            if(isDecimalPoint(input.charAt(i)))
-                decimalPoints++;
+        }catch(NumberFormatException e){
+            return 0;
         }
-
-        if(input.length() != numOperators + numDigits + decimalPoints) {
-            //(input.length() != numOperators + numDigits + decimalPoints) ->significa que existem caraccteres invalidos
-
-            return false;
-
-        }
-
-        if(isOperator(input.charAt(0)) || isOperator(input.charAt(input.length() - 1))
-                || isDecimalPoint(input.charAt(0)) || isDecimalPoint(input.charAt(input.length() - 1)))
-            return false; //verifica se o primeiro e ultimo operadores sao invalidos
-
-
-        for(int i = 0; i < input.length(); i++)
-        {
-            //NÃO É PERCISO PRECUPAR COM OPERAÇOES DO TIPO INDEX = -1 OU INDEX > LENGTH PQ A OPERAÇÃO ANTERIOIR JA FILTROU ISSO
-
-            if(isDecimalPoint(input.charAt(i)))
-            {
-                if(isDecimalPoint(input.charAt(i -1)) || isDecimalPoint(input.charAt(i + 1)))
-                    return false; // verifica se existem 2 "." juntos
-
-                if(isOperator(input.charAt( i - 1)) || isOperator(input.charAt(i + 1)))
-                {
-                    return false; //verifica se existem operadores e pontos juntos "+."
-                }
-            }
-
-            if(isOperator(input.charAt(i)))
-            {
-                if(isOperator(input.charAt(i - 1)) || isOperator(input.charAt(i + 1)))
-                {
-                    return false; // verifica se existem operadores juntos "--"
-                }
-            }
-
-
-        }
-        return true;
-
-
     }
-
 
     public static void main(String[] args) {
         //launch();
@@ -102,19 +46,13 @@ public class HelloApplication extends Application {
             input = in.nextLine();
             if(input.equalsIgnoreCase("sair"))
                 break;
-            if(isValidInput(input))
-                System.out.println("Input valido :)) " + input);
+            if(isValidInput(input) != 0)
+                System.out.println("Input valido : " + isValidInput(input));
             else
-                System.out.println("Input invalido :((( " + input);
-
-
+                System.out.println("Input invalido : " + input);
         }
 
-
         System.out.println("Sai");
-
-
-
-
+        System.exit(0);
     }
 }
