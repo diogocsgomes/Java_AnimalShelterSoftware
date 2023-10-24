9c0c4812c44393ff7aad0f031d3eb1150a10fd11
Bruno Fonseca <a2021129658@isec.pt>
2023-10-24 20:25:34 +0100

    update mockups

--- ShelterWise/src/main/java/com/example/shelterwise/StarterApplication.java
+++ ShelterWise/src/main/java/com/example/shelterwise/StarterApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class StarterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StarterApplication.class.getResource("animais-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StarterApplication.class.getResource("animais-list-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("ShelterWise");
         stage.setScene(scene);
