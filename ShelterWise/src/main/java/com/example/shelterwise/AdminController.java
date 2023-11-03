5558975e581dd57d6bb743d3ad76172224f32080
Bruno Fonseca <a2021129658@isec.pt>
2023-11-03 13:51:25 +0000

    update admin - animais controllers

--- ShelterWise/src/main/java/com/example/shelterwise/AdminController.java
+++ ShelterWise/src/main/java/com/example/shelterwise/AdminController.java
@@ -1,15 +1,20 @@
 package com.example.shelterwise;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AdminController {
+    @FXML
+    private Button btnAnimais;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -29,31 +34,15 @@ public class AdminController {
         stage.show();
     }
     public void switchListaVeterinarios(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
     public void switchVerDoacoes(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
     public void switchListaStock(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
     public void switchVerCalendario(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
 }
--- ShelterWise/src/main/java/com/example/shelterwise/AnimaisController.java
+++ ShelterWise/src/main/java/com/example/shelterwise/AnimaisController.java
@@ -1,4 +1,52 @@
 package com.example.shelterwise;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AnimaisController {
+    @FXML
+    private static Label lblTitulo;
+    private Stage stage;
+    private Scene scene;
+    private static Scene preScene;
+    private Parent root;
+
+    public void switchVoltar(ActionEvent event) throws IOException {
+        Parent root = preScene.getRoot();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(root.getScene());
+        stage.show();
+    }
+    public void switchCriarAnimal(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("animais-info.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        preScene = stage.getScene();
+        scene = new Scene(root);
+        //stage.setTitle("Criar Animal");  Corrigir problema de não mudar o título
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchEditarAnimal(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("animais-info.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        preScene = stage.getScene();
+        scene = new Scene(root);
+        stage.setTitle("Editar Animal");
+        //lblTitulo.setText("Editar Animal");  Corrigir problema de não mudar o título
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void eliminarAnimal(ActionEvent event) throws IOException {
+
+    }
 }
