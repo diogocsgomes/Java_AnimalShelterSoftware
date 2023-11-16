600c78a2df279f1bb991a3aac9e9cc6ca669d55f
Bruno Fonseca <a2021129658@isec.pt>
2023-11-16 13:18:31 +0000

    unit test animals

---
+++ ShelterWise/src/test/java/com/example/shelterwise/TestMethods/TestingAnimalsClassTest.java
@@ -0,0 +1,34 @@
+package com.example.shelterwise.TestMethods;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestingAnimalsClassTest {
+
+    @Test
+    void loadInfoAnimals() {
+
+       TestingAnimalsClass testingAnimalsClass = new TestingAnimalsClass();
+       boolean result = testingAnimalsClass.loadInfoAnimals();
+       assertTrue(result);
+    }
+    @Test
+    void loadInfoAnimalsForName() {
+        TestingAnimalsClass testingAnimalsClass = new TestingAnimalsClass();
+        boolean result = testingAnimalsClass.loadInfoAnimalsForName();
+        assertTrue(result);
+    }
+    @Test
+    void loadInfoAnimalsForType() {
+        TestingAnimalsClass testingAnimalsClass = new TestingAnimalsClass();
+        boolean result = testingAnimalsClass.loadInfoAnimalsForType();
+        assertTrue(result);
+    }
+    @Test
+    void loadInfoAnimalsForNameAndType() {
+        TestingAnimalsClass testingAnimalsClass = new TestingAnimalsClass();
+        boolean result = testingAnimalsClass.loadInfoAnimalsForNameAndType();
+        assertTrue(result);
+    }
+}
