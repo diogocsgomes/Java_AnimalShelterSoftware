ded5241edf6b52a700b2a33d9e4d48e36dd6487a
Ricardo Tavares <a2021144581@isec.pt>
2023-11-29 17:59:57 +0000

    Lista de Veterinarios adicionada a voluntarios

--- src/main/java/com/example/gps_g21/Voluntarios/VoluntariosController.java
+++ src/main/java/com/example/gps_g21/Voluntarios/VoluntariosController.java
@@ -70,4 +70,13 @@ public class VoluntariosController {
         stage.show();
     }
 
+    public void switchListaVeterinarios(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/gps_g21/veterinarios-list-view.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Veterinários");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
