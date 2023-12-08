81afffc6e3dd14162a3886b6f92c2bd8ec9f1dc8
a <76637393+atlasjx@users.noreply.github.com>
2023-12-08 04:18:59 +0000

    Added inscrever em eventos (voluntários

--- src/main/java/com/example/gps_g21/Calendario/CalendarioViewControllerVol.java
+++ src/main/java/com/example/gps_g21/Calendario/CalendarioViewControllerVol.java
@@ -5,9 +5,9 @@ import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarEvent;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
-import com.calendarfx.view.CalendarView;
 import com.calendarfx.view.page.WeekPage;
-import com.example.gps_g21.Modelos.Calendario;
+import com.example.gps_g21.Modelos.SqliteController;
+import com.example.gps_g21.StarterController;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -19,11 +19,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.controlsfx.control.PopOver;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
@@ -31,6 +37,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.calendarfx.model.CalendarEvent.*;
 
@@ -44,9 +51,17 @@ public class CalendarioViewControllerVol {
     private Scene scene;
     private static Scene preScene;
 
+    private String idEntry;
+
+    private int id;
+
+    private String nameStarter = StarterController.loggedName;
 
     private static CalendarioController calendarioController;
     private Timer updateTimer;
+
+    SqliteController sqliteController = new SqliteController();
+    Connection connection = null;
     public void initialize() throws Exception {
         Calendar lavar = new Calendar("Lavar");
         lavar.setStyle(Calendar.Style.STYLE1);
@@ -85,32 +100,53 @@ public class CalendarioViewControllerVol {
         updateTime.start();
 
         List<Calendario> calendario = calendarioController.loadCalendar();
-        if(calendario != null){
+        //AtomicReference<String> idEntry = null;
+        if (calendario != null) {
             for (Calendario c : calendario) {
                 Entry<String> entry = new Entry<>(c.getTitle());
                 LocalDate startDate = LocalDate.parse(c.getStartDate());
                 LocalDate endDate = LocalDate.parse(c.getEndDate());
-                entry.setInterval(startDate.atTime(LocalTime.parse(c.getStartTime())),endDate.atTime(LocalTime.parse(c.getEndTime())));
+                entry.setInterval(startDate.atTime(LocalTime.parse(c.getStartTime())), endDate.atTime(LocalTime.parse(c.getEndTime())));
                 ZoneId zoneId = ZoneId.of(c.getZoneId());
                 entry.setZoneId(zoneId);
                 entry.setFullDay(c.isFullDay());
                 entry.setRecurrenceRule(c.getRecurrenceRule());
                 entry.setId(c.getId());
+                //System.out.println("Id da entry: " + idEntry);
+
                 switch (c.getCalendarName()) {
                     case "Lavar" -> entry.setCalendar(lavar);
                     case "Alimentar" -> entry.setCalendar(alimentar);
                     case "Limpar" -> entry.setCalendar(limpar);
                 }
-                if(c.getIdsVoluntiers() == null){
+                if (c.getIdsVoluntiers() == null) {
                     entry.getStyleClass().add("custom");
                     System.out.println("Cor vermelha");
-                }else{
+                } else {
                     System.out.println("Cor verde");
                 }
                 weekPage.getCalendars().get(0).addEntry(entry);
             }
         }
+
+
+        weekPage.setEntryDetailsPopOverContentCallback(param -> {
+            Entry<String> selectedEntry = (Entry<String>) param.getEntry();
+            String entryId = selectedEntry.getId();
+            System.out.println("Clicked Entry ID: " + entryId);
+
+            idEntry = entryId;
+
+            return null;
+        });
+
+
+
+        idEntry = weekPage.getEntryDetailsPopOverContentCallback().toString();
+        System.out.println(idEntry);
+
     }
+
     private void handleEventCalendario(CalendarEvent event) {
         EventType type = event.getEventType();
         System.out.println("Tipo Evento: " + type);
@@ -120,19 +156,8 @@ public class CalendarioViewControllerVol {
             calendarioController.insertCalendarEvent(event.getEntry());
         } else if (type == ENTRY_INTERVAL_CHANGED || type == ENTRY_TITLE_CHANGED || type == ENTRY_FULL_DAY_CHANGED || type == ENTRY_LOCATION_CHANGED || type == ENTRY_RECURRENCE_RULE_CHANGED  || type == ENTRY_USER_OBJECT_CHANGED || type == ENTRY_CALENDAR_CHANGED) {
             scheduleUpdate(event.getEntry()); //Atrasa a atualização para evitar que muitas atualizações sejam executadas em um curto período de tempo
-            addInscreverButton(event.getEntry());
+            //addInscreverButton(event.getEntry());
         }
-    }
-
-    private void addInscreverButton(Entry calendarEvent) {
-        Button inscreverButton = new Button("Inscrever");
-        //inscreverButton.setOnAction(event -> handleInscreverButton(calendarEvent));
-
-        Label eventLabel = new Label(calendarEvent.getTitle());
-        HBox entryBox = new HBox(eventLabel, inscreverButton);
-
-        // Add the button directly to the entry box
-        //calendarEvent.getControl().setGraphic(entryBox);
     }
 
     private void scheduleUpdate(Entry calendarEvent) {
@@ -160,5 +185,166 @@ public class CalendarioViewControllerVol {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void handleInscreverButton(MouseEvent mouseEvent) throws SQLException {
+        System.out.println("cliquei entry");
+        registerAttendance(idEntry, nameStarter);
+    }
+
+    /*
+    public boolean registerAttendance(String eventId, int userId) throws SQLException {
+        String selectUserSql = "SELECT id FROM users WHERE email = " + nameStarter;
+        String sql = "INSERT INTO attendances (event_id, user_id, n_attendances) VALUES (?, ?, 1)";
+
+        PreparedStatement pstmtUser = connection.prepareStatement(selectUserSql);
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+
+
+        try {
+            connection = sqliteController.createDBConnection();
+
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, eventId);
+            pstmt.setInt(2, userId);
+
+            int rowsAffected = pstmt.executeUpdate();
+
+            // Check if the insertion was successful
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle the exception as needed
+            return false;
+        } finally {
+            // Close the resources
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Handle the exception as needed
+            }
+        }
+    }
+
+     */
+    public boolean registerAttendance(String eventId, String userEmail) throws SQLException {
+        String selectUserSql = "SELECT id FROM users WHERE nome = ?";
+        String selectNAttendancesSql = "SELECT n_attendances FROM attendances WHERE event_id = ?";
+        String selectMaxAttendancesSql = "SELECT maxVoluntiers FROM calendar WHERE id = ?";
+        String insertAttendanceSql = "INSERT INTO attendances (event_id, user_id, n_attendances) VALUES (?, ?, ?)";
+        String updateAttendanceSql = "UPDATE attendances SET n_attendances = ? WHERE event_id = ? AND user_id = ?";
+
+        PreparedStatement selectUserStmt = null;
+        PreparedStatement selectNAttendancesStmt = null;
+        PreparedStatement selectMaxAttendancesStmt = null;
+        PreparedStatement insertAttendanceStmt = null;
+        PreparedStatement updateAttendance = null;
+        ResultSet resultSet = null;
+        ResultSet resNAttendances = null;
+        ResultSet resMaxAttendances = null;
+
+        System.out.println("estou no inicio do registerattendance");
+
+        try {
+            connection = sqliteController.createDBConnection();
+
+            // pega o id do user
+            selectUserStmt = connection.prepareStatement(selectUserSql);
+            selectUserStmt.setString(1, userEmail);
+            resultSet = selectUserStmt.executeQuery();
+
+            int userId;
+
+            System.out.println("procurou id pelo email: " + nameStarter);
+
+            // verifica se há alguém c o email
+            if (resultSet.next()) {
+                System.out.println("verificou e existe email");
+                userId = resultSet.getInt("id");
+
+                //guarda o n de attendances no evento
+                selectNAttendancesStmt = connection.prepareStatement(selectNAttendancesSql);
+                selectNAttendancesStmt.setString(1, eventId);
+                resNAttendances = selectNAttendancesStmt.executeQuery();
+
+                int nAttendances = 0;
+
+                if (resNAttendances.next()) {
+                    nAttendances = resNAttendances.getInt("n_attendances");
+                }
+
+                //guarda o máx de voluntários para um evento
+                selectMaxAttendancesStmt = connection.prepareStatement(selectMaxAttendancesSql);
+                selectMaxAttendancesStmt.setString(1, eventId);
+                resMaxAttendances = selectMaxAttendancesStmt.executeQuery();
+
+                int maxAttendances = 0;
+                if (resMaxAttendances.next()) {
+                    maxAttendances = resMaxAttendances.getInt("maxVoluntiers");
+                }
+
+                if (nAttendances < maxAttendances) {
+                    insertAttendanceStmt = connection.prepareStatement(insertAttendanceSql);
+                    insertAttendanceStmt.setString(1, eventId);
+                    insertAttendanceStmt.setInt(2, userId);
+
+                    nAttendances = nAttendances + 1;
+                    insertAttendanceStmt.setInt(3, nAttendances);
+
+                    int rowsAffected = insertAttendanceStmt.executeUpdate();
+
+                    System.out.println("meti na tabela");
+
+                    return rowsAffected > 0;
+                } else {
+                    System.out.println("Lotação máxima do evento atingida");
+                    return false;
+                }
+            } else {
+                System.out.println("User com este email não encontrado");
+                return false;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        } finally {
+            // Close the resources
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (resNAttendances != null) {
+                    resNAttendances.close();
+                }
+                if (resMaxAttendances != null) {
+                    resMaxAttendances.close();
+                }
+                if (selectUserStmt != null) {
+                    selectUserStmt.close();
+                }
+                if (selectNAttendancesStmt != null) {
+                    selectNAttendancesStmt.close();
+                }
+                if (selectMaxAttendancesStmt != null) {
+                    selectMaxAttendancesStmt.close();
+                }
+                if (insertAttendanceStmt != null) {
+                    insertAttendanceStmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
