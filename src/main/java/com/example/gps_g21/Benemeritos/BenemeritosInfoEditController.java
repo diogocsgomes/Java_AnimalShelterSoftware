e50f182909b57aad2132406e38ad1e1e1c6a2277
Ricardo Tavares <a2021144581@isec.pt>
2023-12-13 18:18:47 +0000

    Editar benemeritos adicionado

--- src/main/java/com/example/gps_g21/Benemeritos/BenemeritosInfoEditController.java
+++ src/main/java/com/example/gps_g21/Benemeritos/BenemeritosInfoEditController.java
@@ -1,8 +1,6 @@
 package com.example.gps_g21.Benemeritos;
 
 import com.example.gps_g21.Modelos.SqliteController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,6 +17,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 public class BenemeritosInfoEditController {
@@ -51,40 +52,84 @@ public class BenemeritosInfoEditController {
             this.id = id;
         }
 
-        public void initialize() throws SQLException {
-            try {
-                connection = sqliteController.createDBConnection();
+    public void initialize() {
+        try {
+            connection = sqliteController.createDBConnection();
 
-                if (connection == null) {
-                    System.out.println("Erro de conexão à base de dados");
-                    System.exit(1);
-                }else {
-                    System.out.println("Db aberta no BenemeritosInfoEditController");
-                }
+            if (connection == null) {
+                System.out.println("Error connecting to the database");
+                System.exit(1);
+            } else {
+                System.out.println("Database connected in BenemeritosInfoEditController");
+            }
 
-                String query = "SELECT * FROM adopters WHERE id = " + id;
+            String query = "SELECT * FROM adopters WHERE id = " + this.id;
 
-                PreparedStatement preparedStatement = connection.prepareStatement(query);
-                ResultSet resultSet = preparedStatement.executeQuery();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
+            if (resultSet.next()) {
                 nome.setText(resultSet.getString("name"));
                 address.setText(resultSet.getString("address"));
-                phone.setText(String.valueOf(resultSet.getInt("phone")));
+                phone.setText(String.valueOf(resultSet.getInt("phone_number")));
                 email.setText(resultSet.getString("email"));
-                // No birth_date in the adopters table according to your information
-                nif.setText(resultSet.getString("nif"));
-            } finally {
-                sqliteController.closeDBConnection(connection);
-                System.out.println("Db fechada no BenemeritosInfoEditController (initialize)");
+
+                String birthDateStr = resultSet.getString("birth_date");
+                if (birthDateStr != null && !birthDateStr.isEmpty()) {
+                    try {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                        LocalDate birthDate = LocalDate.parse(birthDateStr, formatter);
+                        birth_date.setValue(birthDate);
+                    } catch (DateTimeParseException e) {
+                        System.out.println("Error parsing birth date: " + e.getMessage());
+                    }
+                }
+
+                // Set nif value
+                nif.setText(String.valueOf(resultSet.getInt("nif")));
             }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            sqliteController.closeDBConnection(connection);
+            System.out.println("Database closed in BenemeritosInfoEditController (initialize)");
         }
+    }
 
-        @FXML
-        public void save() {
-            // Implement the logic to save changes to the database
+    @FXML
+    public void save(ActionEvent event) throws IOException {
+        String sql = "UPDATE adopters SET name = ?, birth_date = ?, address = ?, phone_number = ?, email = ?, nif = ? WHERE id = ?";
+
+        try (Connection connection = sqliteController.createDBConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, nome.getText());
+            pstmt.setString(2, birth_date.getValue().toString());
+            pstmt.setString(3, address.getText());
+            pstmt.setInt(4, Integer.parseInt(phone.getText()));
+            pstmt.setString(5, email.getText());
+            pstmt.setInt(6, Integer.parseInt(nif.getText()));
+            pstmt.setInt(7,id);
+
+
+            int linhasAfetadas = pstmt.executeUpdate();
+            System.out.println("Linhas afetadas: " + linhasAfetadas);
+            System.out.println("Dados atualizados com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            sqliteController.closeDBConnection(connection);
+
+            // Move the stage initialization here
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/gps_g21/benemeritos-list-view.fxml")));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         }
+    }
 
-        @FXML
+
+    @FXML
         public void goBack(ActionEvent event) throws IOException {
             sqliteController.closeDBConnection(connection);
 
